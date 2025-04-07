59adecb80f51fdbbee620ae596ecae34d820f4c2
chacy239 <chacy239@gmail.com>
2025-04-07 17:23:14 +0800

    Update Chatroom.jsx

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB6KrILbmD6knl9yOsQuFEDP38oU67bcg",
+  authDomain: "chat-seats-chat-room.firebaseapp.com",
+  databaseURL: "https://chat-seats-chat-room-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-seats-chat-room",
+  storageBucket: "chat-seats-chat-room.appspot.com",
+  messagingSenderId: "804236683094",
+  appId: "1:804236683094:web:a8236d8f0f0572acfbd8f7"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default database;
